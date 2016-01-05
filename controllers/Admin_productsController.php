5eb60066fdde5e16d4e6dad392c733f19bc82d2a
dram1008 <dram1008@yandex.ru>
2016-01-05 19:29:43 +0300

    init

--- controllers/Admin_productsController.php
+++ controllers/Admin_productsController.php
@@ -41,7 +41,6 @@ class Admin_productsController extends AdminBaseController
     public function actionEdit($id)
     {
         $model = \app\models\Form\Shop\Product::find($id);
-        VarDumper::dump($id);
         if ($model->load(Yii::$app->request->post()) && $model->update()) {
             Yii::$app->session->setFlash('contactFormSubmitted');
 
