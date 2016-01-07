95aa200f550eb7acacfcf2a1030ed7a9c59b89d1
dram1008 <dram1008@yandex.ru>
2016-01-07 20:48:38 +0300

    init

--- views/products/planet.php
+++ views/products/planet.php
@@ -44,23 +44,6 @@ $this->title = 'Планета «Новая Земля»';
     </div>
 </section>
 
-<!-- Download Section -->
-<section id="download" class="content-section text-center">
-    <div class="download-section" style="
-    border-bottom: 1px solid #87aad0;
-    border-top: 1px solid #87aad0;
-    ">
-        <div class="container">
-            <div class="col-lg-8 col-lg-offset-2" style="color: #000000">
-                <p class="text-center"><img src="/images/icon.png" class="img-center"> </p>
-                <h2>Попробовать</h2>
-                <p>Один билет равен 1 000 благодарностей (руб.) в наш адрес</p>
-                <a href="/price" class="btn btn-default btn-lg">Получить билет</a>
-            </div>
-        </div>
-    </div>
-</section>
-
 <!-- Contact Section -->
 <section id="contact" class="container content-section text-center">
     <div class="row">
@@ -82,21 +65,9 @@ $this->title = 'Планета «Новая Земля»';
             <h2>Поделиться</h2>
             <?= $this->render('../blocks/share', [
                 'url'         => \yii\helpers\Url::current([], true),
-                'image'       => \yii\helpers\Url::to('/images/promo-facebook1.jpg', true),
-                'title'       => 'Проект «Крылья Ангела». Летайте как птицы! Получите билет от нас в подарок!',
-                'description' => 'Проект Крылья Ангела представляет собой тренажер для полетов в виртуальной реальности вместе с кристальным звуком. По факту этот тренажер является персональным кинотеатром 5D.
-
-Он будет установлен в Москве в марте-мае 2016 г. и стоять в одном из торговых центров Москвы.
-
-Для того чтобы испытать этот тренажер вам нужно получить билет.
-
-Получая билет вы получите:
-- 30 мин первокласного ощущения полета на Крыльях Ангела;
-- измененное состояние сознания легкости после полета;
-- эффект присутствия через зрение, слух, гравитацию, ощущение ветра;
-- фото вашего полета на память;
-- купон на скидку для вас и ваших друзей;
-- специальный подарок!',
+                'image'       => \yii\helpers\Url::to('/images/controller/products/planet/globus.jpg', true),
+                'title'       => 'Планета «Новая Земля»',
+                'description' => 'Планета «Новая Земля» является прототипом Новой Земли, которая сейчас проявляется на Земле. На планету нанесены в виде кристаллов две лей линии и 12 планетарных чакр за исключением 6 чакры которая не имеет фиксированного расположния.',
             ]) ?>
 
 
