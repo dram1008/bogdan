b9f2b843e515fa4b1daa702c7cd8a7a5b8bac13a
dram1008 <dram1008@yandex.ru>
2016-02-28 17:23:27 +0300

    init

--- views/site/buy.php
+++ views/site/buy.php
@@ -47,7 +47,12 @@ $this->registerJs(<<<JS
     $('.buttonSubmit').click(function() {
         // проверка формы
         var val = $('input[name="Request[dostavka]"]').val();
+        $('input[name="Request[dostavka]"]').each(function(i,v) {
+            console.log($(v).is(':checked'));
+            console.log($(v).is(':selected'));
+        });
         console.log(val);
+        return false;
         if (val == '') {
             $('.field-request-dostavka').addClass('has-error');
             $('.field-request-dostavka .help-block').html('Поле нужно выбрать обязательно').show();
